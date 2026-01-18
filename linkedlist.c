e887fe06b6d703aab5c97863e1719c4e85343031
Anh Tu Do <anhtu.do1998@gmail.com>
2026-01-18 22:23:29 +0800

    Added some linked list functionality to be implemented

--- linkedlist.c
+++ linkedlist.c
@@ -2,7 +2,7 @@
 
 typedef struct IntNode {
     int val;
-    struct IntNode * next;
+    struct IntNode * next; // within the struct, no idea that IntNode_t is there yet
 } IntNode_t;
 
 void print_list(IntNode_t * head_ptr) {
@@ -15,11 +15,37 @@ void print_list(IntNode_t * head_ptr) {
     printf("\n");
 }
 
+IntNode_t* reverse_list(IntNode_t* head) {
+    return NULL;
+}
+
+// TODO: Is it common to just rely on the head_ptr as per previous?
+
+void push_front(IntNode_t* head, int value) {
+    return;
+}
+
+void push_back(IntNode_t* head, int value) {
+    return;
+}
+
+int pop_back(IntNode_t* head) {
+    return 0; // TODO
+}
+
+int pop_front(IntNode_t* head) {
+    return 0;
+}
+
 int main() {
     IntNode_t node = {5, NULL};
     IntNode_t node2 = {10, NULL};
     IntNode_t node3 = {15, NULL};
     node.next = &node2;
     node2.next = &node3;
-    print_list(&node);
+    IntNode_t *head = &node;
+    print_list(head);
+
+    IntNode_t * reversed = reverse_list(head);
+    print_list(reversed);
 }
